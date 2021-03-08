dc8810a507e764ab31714d0570860bb79bf45c78
Evgeny Safonov <safonov.e.s@gmail.com>
2021-03-08 20:23:20 +0300

    .

--- .eslintrc.js
+++ .eslintrc.js
@@ -140,7 +140,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      "jsx": true
+      jsx: true
     },
     ecmaVersion: 2021,
     sourceType: 'module',
